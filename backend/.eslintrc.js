6ffdb7f955f761ef847ddad2f514c355e2461072
hunainqureshi-10p <hunain.qureshi@tenpearls.com>
2019-11-24 20:58:16 +0500

    Backend setup

---
+++ backend/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  plugins: [
+    '@typescript-eslint',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  rules: {
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': [
+      'error', {
+         'devDependencies': true,
+      }
+    ],
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off'
+  }
+};
---
+++ backend/src/app.ts
@@ -0,0 +1,9 @@
+import Koa from 'koa';
+
+const app = new Koa();
+
+app.use(async (ctx: { body: string }) => {
+  ctx.body = 'abdul-kareem';
+});
+
+app.listen(3000, () => console.log('Server started...'));
